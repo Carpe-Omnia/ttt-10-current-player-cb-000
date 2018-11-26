40074d162063178156252ec04e67aed1cbd5ebbe
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-11-26 19:19:34 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,21 @@
+def turn_count(board)
+  turn = 0
+  board.each do |spot|
+    if spot == " " || spot == "" || spot = nil
+      turn += 0
+    else
+      turn += 1
+    end
+  end
+  return turn
+end
+
+def current_player(board)
+  turns_elapsed = turn_count(board)
+  if turns_elapsed % 2 != 0
+    player = "O"
+  else
+    player = "X"
+  end
+  return player
+end
